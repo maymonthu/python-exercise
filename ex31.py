971fc14ce4a4465b366a87b65931f188ad5832ee
maymonthu <maymonthu9736@gmail.com>
2018-06-17 22:12:34 +0630

    adding ex.py

--- ex31.py
+++ ex31.py
@@ -1,10 +1,10 @@
 print("""You enter a dark room with two doors.
         Do you go through door #1 or door #2?""")
 
-door = input("> ")
+door=input("> ")
 
 if door == "1":
-    print("There's a giant bear here eating a cheese cake.")
+    print("There's a gaint bear here eating a cheese cake.")
     print("What do you do?")
     print("1. Take the cake.")
     print("2. Scream at the bear.")
@@ -19,20 +19,20 @@ if door == "1":
         print(f"Well, doing {bear} is probably better.")
         print("Bear runs away.")
 
-    elif door == "2":
-        print("You stare into the endless abyss at Cthulhu's retina.")
-        print("1. Blueberries.")
-        print("2. Yellow jacket clothespins.")
-        print("3. Understanding revolvers yelling melodies.")
+elif door == "2":
+    print("You stare into the endless abyss at Cthulhu's ret.")
+    print("1. Blueberries.")
+    print("2. Yellow jacket clothespins.")
+    print("3. Understanding revolvers yelling melodies.")
 
-        insanity = input("> ")
+    insanity = input("> ")
 
-        if insanity == "1" or insanity == "2":
-            print("Your body survives powered by a mind of jello.")
-            print("Good job!")
-        else:
-            print("The insanity rots your eyes into a pool of muck.")
-            print("Good job!")
+    if insanity == "1" or insanity == "2":
+        print("Your body survives powered by a mind of jello.")
+        print("Good job!")
+    else:
+        print("The insanity rots your eyes into a pool of mud.")
+        print("Good job!")
 
-        else:
-            print("You stumble around and fall on a knife and die. Good job!")
+else:
+    print("You stumble around and fall on a knife and die. Good job!")
